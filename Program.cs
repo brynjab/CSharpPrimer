5205d2ea6c3ecc83d42ece5a84ad545e5472dabd
unknown <bbg1@nemandi.ntv.is>
2022-02-23 21:55:33 +0000

    ...

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace CSharpPrimer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Challenge 1
+            
+            List<int> numbers = new List<int>();
+            string input = "x";
+            int number;
+
+            while(input != "")
+            {   
+               Console.WriteLine("Please enter a number");
+               input = Console.ReadLine();
+                bool parse = int.TryParse(input, out number);
+
+                if (parse)
+                {
+                    numbers.Add(number);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            int max = numbers.Max();
+            int min = numbers.Min();
+            Console.WriteLine($"Largest value: {max}");
+            Console.WriteLine($"Smallest value: {min}");
+            
+
+            // Challenge 2
+            
+            WeatherData weather = new WeatherData(100, 23,"F");
+            Console.Write("Would you like to convert? (Y/N)");
+            string answer = Console.ReadLine();
+            if(answer == "Y")
+            {
+                weather.Convert();
+                Console.WriteLine(weather.Temperature);
+            } 
+            else if(answer == "N")
+            {
+                Console.WriteLine("K thx BYE!");
+            }
+        }
+    }
+}
---
+++ WeatherData.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPrimer
+{
+    internal class WeatherData
+    {
+        
+        public WeatherData(double temperature, double humidity, string scale)
+        {
+            Temperature = temperature;
+            Humidity = humidity;
+            Scale = scale;
+
+           if(Scale == "C")
+            {
+                if(Temperature > 60 || Temperature < -60)
+                {
+                    Console.WriteLine("Mistake must have been made since the value seems unrealistic");
+                }
+                else
+                {
+                    Console.WriteLine($"Temperature: {Temperature} Celsius"); 
+                }
+                
+            }
+           else if (Scale == "F")
+            {
+                if (Temperature > 140 || Temperature < -76)
+                {
+                    Console.WriteLine("Mistake must have been made since the temperature value seems unrealistic");
+                }
+                else
+                {
+                    Console.WriteLine($"Temperature: {Temperature} Fahrenheit");
+                }
+            }
+           else
+            {
+                Console.WriteLine("Unknown Scale");
+            }
+
+            if (Humidity < 0 || Humidity > 100)
+            {
+                Console.WriteLine("Humidity must be from 0-100%");
+            }
+            else
+            {
+                Console.WriteLine($"Humidity: {Humidity}%");
+            }
+          
+        }
+
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }
+        public string Scale { get; set; }
+
+        public void Convert()
+        {
+            
+
+           if(Scale != "C")
+            {
+                Temperature = (Temperature - 32) * (5 / 9);
+                Scale = "C";
+            } else
+            {
+                Temperature = (Temperature * 9 / 5) + 32;
+                Scale = "F";
+            }
+        }
+    }
+}
